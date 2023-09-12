c63e5e1d6617ac68769b8852dec707dbf1b14478
jimmyw26 <144463463+jimmyw26@users.noreply.github.com>
2023-09-12 01:47:45 -0400

    Add files via upload

---
+++ 1.2 variables and data types.java
@@ -0,0 +1,15 @@
+public class Main 
+{
+  public static void main(String[] args) 
+  {
+    int games;
+    games = 1;
+    System.out.println(games);
+    double money;
+    money = 7.88;
+    System.out.println(money);
+    boolean snowing;
+    snowing = true;
+    System.out.println(snowing);
+  }
+}
