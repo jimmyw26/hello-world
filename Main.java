6d2883a25118703da7352f7f6838dd1e978c1f6c
jimmyw26 <144463463+jimmyw26@users.noreply.github.com>
2023-09-18 00:22:43 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-    System.out.print("Hello World");
-  }
-}
+    public static void main(String[] args)
+    {
+        System.out.print("Enter a number: ");
+        Scanner scan = new Scanner(System.in);
+        String number = scan.nextLine();
+        int x = 5; // int allows for an integer to be initialized
+        int num = 20;
+        x ++; // adds 1 to x
+        double t = 5.0; // double allows for decimals to be initialized
+        double p = num += x;
+        boolean z = true; // boolean can be true or false
+
+        System.out.println("Hello and welcome!");
+        System.out.println(p);
+        System.out.print(number);
+
+        }
+    }
