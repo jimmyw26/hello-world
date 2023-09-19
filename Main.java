8b56eb8abcc301cace15a77625a7e1f3374927a3
jimmyw26 <144463463+jimmyw26@users.noreply.github.com>
2023-09-18 23:50:07 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,19 +3,50 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args)
     {
-        System.out.print("Enter a number: ");
-        Scanner scan = new Scanner(System.in);
-        String number = scan.nextLine();
-        int x = 5; // int allows for an integer to be initialized
-        int num = 20;
-        x ++; // adds 1 to x
-        double t = 5.0; // double allows for decimals to be initialized
-        double p = num += x;
-        boolean z = true; // boolean can be true or false
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Java Calculator\n");
+        System.out.println("Addition(1) Subtraction(2) Multiplication(3) Division(4)\n");
+        System.out.println("What kind of calculation would you like to do: ");
+        int calculation = scanner.nextInt();
+        if (calculation == 1) {
+            System.out.println("Enter your first number: ");
+            int x = scanner.nextInt();
+            System.out.println("Enter your second number: ");
+            int y = scanner.nextInt();
+            int sum = x + y;
+            System.out.print("Your sum will be: " + sum);
 
-        System.out.println("Hello and welcome!");
-        System.out.println(p);
-        System.out.print(number);
+        }
+        if (calculation == 2) {
+            System.out.println("Enter your first number: ");
+            int x = scanner.nextInt();
+            System.out.println("Enter your second number: ");
+            int y = scanner.nextInt();
+            int difference = x / y;
+            System.out.print("Your difference will be " + difference);
+
+        }
+        if (calculation == 3) {
+            System.out.println("Enter your first number: ");
+            int x = scanner.nextInt();
+            System.out.println("Enter your second number: ");
+            int y = scanner.nextInt();
+            int product = x * y;
+            System.out.print("Your product will be " + product);
+
+
+        }
+        if (calculation == 4) {
+            System.out.println("Enter your first number: ");
+            int x = scanner.nextInt();
+            System.out.println("Enter your second number: ");
+            int y = scanner.nextInt();
+            int quotient = x / y;
+            int remainder = x % y;
+            System.out.print("Your quotient will be " + quotient + " with a remainder of " + remainder);
+
+    }
+
 
         }
     }
