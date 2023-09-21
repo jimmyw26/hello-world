f914b8cbfa7e1c8a9a40b89c1ff358f355490ec6
jimmyw26 <144463463+jimmyw26@users.noreply.github.com>
2023-09-21 10:03:48 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,16 @@
+import java.util.Objects;
 import java.util.Scanner;
 
+import static java.lang.System.exit;
+
 public class Main {
-    public static void main(String[] args) {
+    public static <ArithemticException extends Throwable> void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the Java Calculator\n");
         System.out.println("Addition(1) Subtraction(2) Multiplication(3) Division(4)\n");
         System.out.println("What kind of calculation would you like to do: ");
-        int calculation = scanner.nextInt();
-        if (calculation == 1) {
+        String calculation = scanner.nextLine();
+        if (Objects.equals(calculation, "1")) {
             System.out.println("Enter your first number: ");
             int x = scanner.nextInt();
             System.out.println("Enter your second number: ");
@@ -15,37 +18,43 @@ public class Main {
             int sum = x + y;
             System.out.print("Your sum will be: " + sum);
 
-        } else if (calculation == 2) {
+        } else if (Objects.equals(calculation, "2")) {
             System.out.println("Enter your first number: ");
             int x = scanner.nextInt();
             System.out.println("Enter your second number: ");
             int y = scanner.nextInt();
-            int difference =  x - y;
+            int difference = x - y;
             System.out.print("Your difference will be " + difference);
 
-        } else if (calculation == 3) {
+        } else if (Objects.equals(calculation, "3")) {
             System.out.println("Enter your first number: ");
             int x = scanner.nextInt();
             System.out.println("Enter your second number: ");
             int y = scanner.nextInt();
             int product = x * y;
             System.out.print("Your product will be " + product);
-        } else if (calculation == 4) {
+        } else if (Objects.equals(calculation, "4")) {
             System.out.println("Enter your first number: ");
             int x = scanner.nextInt();
             System.out.println("Enter your second number: ");
             int y = scanner.nextInt();
-            int quotient = x / y;
-            int remainder = x % y;
-            System.out.print("Your quotient will be " + quotient + " with a remainder of " + remainder);
+            if (y == 0) {
+                try {
+                    int ae = x / y;
+                } catch (ArithmeticException ae) {
+                    System.out.println("Your answer will be undefined");
+                    exit(0);
+                }
+                int quotient = x / y;
+                int remainder = x % y;
+                System.out.print("Your quotient will be " + quotient + " with a remainder of " + remainder);
+
+            } else {
+
+                System.out.println(calculation + " isn't one of the options");
+
+            }
 
         }
-
-        else  {
-
-            System.out.println(calculation + " isn't one of the options");
-
-        }
-
     }
 }
