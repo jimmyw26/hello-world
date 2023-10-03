82f93a549067ed8d0b0712b5b46edf4e8d91ba63
jimmyw26 <144463463+jimmyw26@users.noreply.github.com>
2023-10-03 10:22:34 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,11 @@
+
 import java.util.Objects;
 import java.util.Scanner;
 
 import static java.lang.System.exit;
 
 public class Main {
-    public static <ArithemticException extends Throwable> void main(String[] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         startGame s = new startGame();
         s.intro();
@@ -14,42 +15,31 @@ public class Main {
             int x = scanner.nextInt();
             System.out.println("Enter your second number: ");
             int y = scanner.nextInt();
-            s.add(x,y);
+            s.add(x, y);
 
         } else if (Objects.equals(operation, "2")) {
             System.out.println("Enter your first number: ");
             int x = scanner.nextInt();
             System.out.println("Enter your second number: ");
             int y = scanner.nextInt();
-            s.sub(x,y);
+            s.sub(x, y);
 
         } else if (Objects.equals(operation, "3")) {
             System.out.println("Enter your first number: ");
             int x = scanner.nextInt();
             System.out.println("Enter your second number: ");
             int y = scanner.nextInt();
-            s.mult(x,y);
+            s.mult(x, y);
         } else if (Objects.equals(operation, "4")) {
             System.out.println("Enter your first number: ");
             int x = scanner.nextInt();
             System.out.println("Enter your second number: ");
             int y = scanner.nextInt();
-            if (y == 0) {
-                try {
-                    int ae = x / y;
-                } catch (ArithmeticException ae) {
-                    System.out.println("Your answer will be undefined");
-                    exit(0);
-                }
-                int quotient = x / y;
-                int remainder = x % y;
-                System.out.print("Your quotient will be " + quotient + " with a remainder of " + remainder);
+            s.div(x, y);
 
-            } else {
+        } else {
 
-                System.out.println(operation + " isn't one of the options");
-
-            }
+            System.out.println("Not one of the options");
 
         }
     }
